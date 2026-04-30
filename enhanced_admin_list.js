adc8027dd429a071258af6f1add86ca21239ecb3
aka-FinGo <akafingo@gmail.com>
2026-04-30 20:26:39 +0500

    UP

--- enhanced_admin_list.js
+++ enhanced_admin_list.js
@@ -3,8 +3,9 @@
  * Improved UI with compact list and modal details
  */
 
-const ADMIN_ITEMS_PER_PAGE = 50;
+const ADMIN_ITEMS_PER_PAGE = 15;
 let adminCurrentPage = 1;
+let filteredData = [];
 
 // Escape HTML to prevent XSS
 function escapeHtml(text) {
